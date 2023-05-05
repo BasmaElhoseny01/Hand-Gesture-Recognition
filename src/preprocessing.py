709653138ec549e5326b4135628a16e429c993cd
BasmaElhoseny <basmaelhoseny6@gmail.com>
2023-05-05 14:42:49 +0300

    Find Contours for the Skin Image

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,12 +2,39 @@ from utils import *
 import utils
 
 
-def preprocessing(img, name=""):
+def preprocessing(img, name="", debug=False):
     '''
     @parm img:BGR Scale img
     '''
-    skin = skin_masks(img, name)
-    return skin
+    skin = skin_masks(img, name, debug)
+
+    # Find Contours
+    contours, hierarchy = cv2.findContours(
+        skin, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+
+    # Draw for debug
+    if (debug):
+        img_contours = np.copy(img)
+        cv2.drawContours(img_contours, contours, -1, (0, 255, 0), 3)
+
+    # Get Largest Contour
+    sorted_contours = sorted(contours, key=cv2.contourArea, reverse=True)
+    largest_contour = sorted_contours[0]
+    if (debug):
+        largest_contour_img = np.copy(img)
+        cv2.drawContours(largest_contour_img,
+                         largest_contour, -1, (255, 0, 0), 10)
+
+    # Binary_img_contours[Result]
+    hand_contour = np.zeros((np.shape(img)[0], np.shape(img)[1], 1))
+    cv2.drawContours(hand_contour, largest_contour, -1, 255, 10)
+
+    if (debug):
+        show_images([cv2.cvtColor(img, cv2.COLOR_BGR2RGB),
+                    skin, img_contours, largest_contour_img, hand_contour],
+                    ['RGB'+name, 'Mask(3)YCrCb', 'Contours', 'Largest Contour', 'hand_contour'])
+
+    return hand_contour
 
 
 def remove_shadow(img):
@@ -28,7 +55,7 @@ def remove_shadow(img):
     return None
 
 
-def skin_masks(img_bgr, name=""):
+def skin_masks(img_bgr, name="", debug=False):
     '''
     Gets different masks for the skin color on the image
     @parm img_bgr:BGR Scale img
@@ -60,8 +87,8 @@ def skin_masks(img_bgr, name=""):
 
     # show_images([cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB), HSV_mask, skinRegionYCrCb,
     #             YCrCb_mask, ], ['RGB'+name, 'Mask(1) HSV', 'Mask(2)YCrCb', 'Mask(3)YCrCb'])
-
-    show_images([cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB),
-                YCrCb_mask, ], ['RGB'+name, 'Mask(3)YCrCb'])
+    # if(debug):
+    # show_images([cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB),
+    #             YCrCb_mask, ], ['RGB'+name, 'Mask(3)YCrCb'])
 
     return YCrCb_mask
