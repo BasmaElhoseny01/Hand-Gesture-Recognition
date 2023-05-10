b1de59e3db2100d74aa8cf1dc22f1f52974d8cef
BasmaElhoseny <basmaelhoseny6@gmail.com>
2023-05-10 05:20:47 +0300

    Ran preprocessing2 with orb

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -1,7 +1,7 @@
 from utils import *
 
 
-def extract_features(path_data_folder, clusters, debug=False, images=None, train=False, visual_words=None):
+def extract_features(path_data_folder, clusters, debug=False, images=None, train=False, visual_words=None, orb_params=None):
     """
     clusters:Number of clusters for the Visual bag Kmeans clustering
     """
@@ -18,21 +18,29 @@ def extract_features(path_data_folder, clusters, debug=False, images=None, train
 
     # Step(2) Descriptors
     # SIFT
-    descriptor_list, sift_vectors = sift_descriptors(images, debug=debug)
+    # descriptor_list, sift_vectors = sift_descriptors(images, debug=False)
 
+    # ORB
+    descriptor_list, orb_vectors = orb_descriptors(
+        images, orb_params, debug=False)
+
+    if debug:
+        print('Description Done')
     # #Step(3) Visual Dictionary
     # #CHECK: To create visual dictionary, we only use train dataset.
     if (train):
         visual_words = kmeans_visual_words(
             clusters, descriptor_list)  # Centroids of the K clusters
         # Till here we  have Defined to have a feature vector of size = clusters
-
+    if debug:
+        print('Kmeans DONE')
     # # #Step(4) Feature Vector
     feature_vectors, classification = image_feature_vectors(
-        sift_vectors, visual_words)
+        orb_vectors, visual_words)
     # # Here We have Achieved Feature Vector size k :D for all images
-
-    # Conver Feature_vecrots from dictionary 
+    if debug:
+        print('Feature vector DONE')
+    # Conver Feature_vecrots from dictionary
 
     return feature_vectors, classification, visual_words
 
@@ -107,7 +115,8 @@ def sift_descriptors(images, debug=False):
     sift = cv2.SIFT_create()
 
     # Dictionary {'0':[[keypts_of_img1*128],[keypts_of_img2*128],[keypts_of_img3*128]..]}
-    sift_vectors = {'0':None,'1':None,'2':None,'3':None,'4':None,'5':None}
+    sift_vectors = {'0': None, '1': None,
+                    '2': None, '3': None, '4': None, '5': None}
     descriptor_list = []  # Total number of keypoints for all data set images * 128
 
     for key, value in images.items():
@@ -137,9 +146,55 @@ def sift_descriptors(images, debug=False):
         # print("shape", (class_descriptors[0]))
 
         # print(sift_vectors[key])
-        sift_vectors[key]=class_descriptors  # [[keypts_img1*128],[keypts_img2*128],[keypts_img3*128]]
+        # [[keypts_img1*128],[keypts_img2*128],[keypts_img3*128]]
+        sift_vectors[key] = class_descriptors
         # print(sift_vectors[key])
     return descriptor_list, sift_vectors
+
+
+def orb_descriptors(images, params=None, debug=False):
+    # initialize ORB detector with custom parameters
+    if params is not None:
+        orb = cv2.ORB_create(params[0], params[1], params[2])
+        #                    nFeatures, scaleFactor, nLevels
+    else:
+        orb = cv2.ORB_create()
+    # Dictionary {'0':[[keypts_of_img1*128],[keypts_of_img2*128],[keypts_of_img3*128]..]}
+    orb_vectors = {'0': None, '1': None,
+                   '2': None, '3': None, '4': None, '5': None}
+    descriptor_list = []  # Total number of keypoints for all data set images * 128
+
+    for key, value in images.items():
+        # Category by Category 0,1,..5
+        if (debug):
+            print("orb_descriptors for "+key)
+
+        class_descriptors = []
+        i = 0
+        for img in value:
+            # Loop over Dictionary items value by value M0 M1 M2
+            # Every image in the category we are in
+            keypoints, descriptors = orb.detectAndCompute(img, None)
+            if (debug):
+                print(str(i)+" has: " + str(len(keypoints)) + "Keypoints")
+                i = i+1
+
+            # Normalize descriptors
+            if descriptors is None:
+                print("No Key Points Detected")
+
+            descriptor_list.extend(descriptors)
+            class_descriptors.append(descriptors)
+
+        # print("shape", np.shape(class_descriptors))
+        # print("shape", np.shape(class_descriptors[0]))
+        # print("shape", (class_descriptors[0]))
+
+        # print(sift_vectors[key])
+        # [[keypts_img1*128],[keypts_img2*128],[keypts_img3*128]]
+        orb_vectors[key] = class_descriptors
+        # print(sift_vectors[key])
+    return descriptor_list, orb_vectors
 
 
 def image_feature_vectors(bag_of_words, centroids):
@@ -154,27 +209,26 @@ def image_feature_vectors(bag_of_words, centroids):
     dict_feature = {}
     classification = np.array([])
     for key, value in bag_of_words.items():
-        print(key, np.shape(value))
+        # print(key, np.shape(value))
         # Each Category
         category = []
         for img in value:
             # Each Image in Category
             ind, histogram = findClosestCentroids(X=img, centroids=centroids)
             category.append(histogram)  # Add this histogram to this Category
-            classification=np.append(classification,key)
+            classification = np.append(classification, key)
         dict_feature[key] = category
 
-
-    #Dictionary rto the array
-    all_feature_descriptors=[]
+    # Dictionary rto the array
+    all_feature_descriptors = []
 
     for fd in dict_feature.values():
-    # fd=fd.reshape(-1,8)
+        # fd=fd.reshape(-1,8)
         for fv in fd:
             # print(fv)
             all_feature_descriptors.append(fv)
 
-    all_feature_descriptors=np.squeeze(all_feature_descriptors)
+    all_feature_descriptors = np.squeeze(all_feature_descriptors)
 
     return all_feature_descriptors, classification
 
