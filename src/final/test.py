2736a1ee7808efa80b823f03ca7f318990f34f1a
BasmaElhoseny <basmaelhoseny6@gmail.com>
2023-05-17 20:01:14 +0300

    Added 4 thread for run

--- src/final/test.py
+++ src/final/test.py
@@ -1,42 +1,119 @@
-import os
-import sys
-# This is the main pipeline for testing our model
+import time
 
+import sys
+import os
 # Step(0) Import the required utilities
 current_dir=os.path.abspath('')
 print("Current Directory: ",current_dir)
 src_dir=os.path.join(current_dir,'../')
 sys.path.insert(1, src_dir) # back to the src directory
 
-from utils import *
-from global_ import IMG_SIZE
-from modules.preprocessing import preprocess_img
 from modules.feature_extraction import extract_features_img
-import time
+from modules.preprocessing import preprocess_img
+from global_ import IMG_SIZE
+from utils import *
+import os
+import sys
+from threading import Thread
+
+# This is the main pipeline for testing our model
+
+# Step(0) Import the required utilities
+current_dir = os.path.abspath('')
+print("Current Directory: ", current_dir)
+src_dir = os.path.join(current_dir, '../')
+sys.path.insert(1, src_dir)  # back to the src directory
+
+
+
+
+def ocr_thread(img, model, debug):
+    # Step-1 Preprocessing
+    img = preprocess_img(img, "2", debug=debug)
+
+    # Step-2 Extract features
+    fv = extract_features_img(img, "OCR")
+
+
+    # Step-3 Predict
+    result = model.predict([fv])
+
+    return result[0]
+
+
+def mask_thread(img, model, debug):
+
+    # Step-1 Preprocessing
+    img = preprocess_img(img, "yasmine1", debug=debug)
+   
+
+    # Step-2 Extract features
+    fv = extract_features_img(img, "hog")
+
+
+    # Step-3 Predict
+    result = model.predict([fv])
+
+
+    return result[0]
+
+class ThreadWithReturnValue(Thread):
+    
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs={}, Verbose=None):
+        Thread.__init__(self, group, target, name, args, kwargs)
+        self._return = None
+
+    def run(self):
+        if self._target is not None:
+            self._return = self._target(*self._args,
+                                                **self._kwargs)
+    def join(self, *args):
+        Thread.join(self, *args)
+        return self._return
+    
 def main(argv):
-    preprocessing_option=argv[0]
-    feature_extractor_option=argv[1]
-    model_option=argv[2]
+    preprocessing_option = argv[0]
+    feature_extractor_option = argv[1]
+    model_option = argv[2]
 
     final_results = []
     time_vector = []
-    
-    #Step-0 Load model
-    print('Loading Model ....')
-    if(model_option=="svm"):
-        filename = "./Trained_SVM.joblib"
-    elif(model_option=="rf"):  
-        filename = "./Trained_RF.joblib"
-    else:
-        print("Wrong Model Option!!!",model_option)
-        raise TypeError("Wrong Feature Option")
-    model = joblib.load(filename)
-    if(model is None):
-        print('Model is None')
-    
+
+    # Step-0 Load models
+    print('Loading Model OCR_SVM ....')
+    filename = "./Trained_OCR_SVM.joblib"
+    ocr_svm_model = joblib.load(filename)
+
+
+    print('Loading Model OCR_RF ....')
+    filename = "./Trained_OCR_RF.joblib"
+    ocr_rf_model = joblib.load(filename)
+
+    print('Loading Model Mask_SVM ....')
+    filename = "./Trained_MASK_SVM.joblib"
+    svm_model = joblib.load(filename)
+
+    print('Loading Model MASK_RF ....')
+    filename = "./Trained_MASK_RF.joblib"
+    rf_model = joblib.load(filename)
+
+    print("Models Loaded Successfully :D")
+
+    # if(model_option=="svm"):
+    #     filename = "./Trained_SVM.joblib"
+    # elif(model_option=="rf"):
+    #     filename = "./Trained_RF.joblib"
+    # else:
+    #     print("Wrong Model Option!!!",model_option)
+    #     raise TypeError("Wrong Feature Option")
+    # model = joblib.load(filename)
+    # if(model is None):
+    #     print('Model is None')
 
     path_data_folder = './data'
     for filename in os.listdir(path_data_folder):
+        fre_array=np.zeros((6,1))
         try:
             # Get path
             path = path_data_folder + "/" + filename
@@ -45,29 +122,51 @@ def main(argv):
             if img is None:
                 continue
 
-            start=time.time()
-
-            #Resize Image
+            start = time.time()
+            # Resize Image
             img = cv2.resize(img, IMG_SIZE)
 
-            #Step-1 Preprocessing
-            img = preprocess_img(img, preprocessing_option, debug=False)
-    
-            #Step-2 Extract features
-            fv = extract_features_img(img,feature_extractor_option)
+            # Create 3 Threads
+            # Start a new thread to Receive messages
 
-            #Step-3 Predict
-            result = model.predict([fv])
-            end = time.time()
+            # th1= ThreadWithReturnValue(target=ocr_thread, args=(img, ocr_svm_model, False))
+            th2= ThreadWithReturnValue(target=ocr_thread, args=(img, ocr_rf_model, False))
+            th3= ThreadWithReturnValue(target=mask_thread, args=(img, rf_model, False))
+            th4= ThreadWithReturnValue(target=mask_thread, args=(img, svm_model, False))
+
+            # th1.start()
+            th2.start()
+            th3.start()
+            th4.start()
 
 
-            final_results.append(result[0])
+            # v1=th1.join()
+            v2=th2.join()
+            v3=th3.join()
+            v4=th4.join()
+            
+
+            # fre_array[v1]+=1
+            fre_array[v2]+=1
+            fre_array[v3]+=1
+            fre_array[v4]+=1
+
+
+            if(np.max(fre_array)==1):
+                result=v4
+            else:
+                result=np.argmax(fre_array)
+            end=time.time()
+
+            print(v2,v3,v4,result)
+
+            final_results.append(result)
             time_vector.append(end-start)
         except Exception as e:
             print('An exception occured in stage: ', str(e))
             final_results.append(-1)
             time_vector.append(-1)
-    
+
     ############
     # Step-4 Write the results into txt file
     y = ["{}\n".format(i) for i in final_results]
@@ -78,7 +177,6 @@ def main(argv):
     with open('./time.txt', 'w') as fp:
         fp.writelines(y)
     print('Results saved')
-
 
 
 if __name__ == "__main__":
