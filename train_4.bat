b5fa46d5dc9054725e3e426c63e80cbd639b4f43
Mohab <hoobamz@gmail.com>
2023-05-17 21:55:04 +0300

    Batch file fixed

--- train_4.bat
+++ train_4.bat
@@ -1,11 +1,13 @@
 cd .\src\
 py .\train.py 2 OCR both
+cd ..
 cd .\models
 ren Trained_RF.joblib Trained_OCR_RF.joblib
 ren Trained_SVM.joblib Trained_OCR_SVM.joblib
 cd ..
 cd .\src\
 py .\train.py yasmine1 hog both
+cd ..
 cd .\models
 ren Trained_RF.joblib Trained_MASK_RF.joblib
 ren Trained_SVM.joblib Trained_MASK_SVM.joblib
